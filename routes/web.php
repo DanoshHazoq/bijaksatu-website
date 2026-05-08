545091b8c9615a2e35bdc5ebe799b9fdb06ed27f
Danish Haziq <145548158+DanoshHazoq@users.noreply.github.com>
2026-05-08 12:05:14 +0800

    Restore named routes

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,26 @@ use App\Http\Controllers\StudentRegistrationController;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/courses', function () {
     return view('courses');
-});
+})->name('courses');
 
 Route::get('/trainers', function () {
     return view('trainers');
-});
+})->name('trainers');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
-Route::get('/register', [StudentRegistrationController::class, 'create']);
-Route::post('/register', [StudentRegistrationController::class, 'store']);
+Route::get('/register', [StudentRegistrationController::class, 'create'])
+    ->name('register');
+
+Route::post('/register', [StudentRegistrationController::class, 'store'])
+    ->name('register.store');
